5dd1774b863f49c334ab9405cbc9c7b89b69dbd7
adaisti <adahyvarinen@1bqqfry.kumpula.dhcp.hupnet.helsinki.fi>
2015-01-20 17:34:29 +0200

    Tämä on vielä pientä

--- Kaannin/src/main/java/ada/kaannin/suomi/Nomini.java
+++ Kaannin/src/main/java/ada/kaannin/suomi/Nomini.java
@@ -42,123 +42,91 @@ public abstract class Nomini extends Taipuva {
         this.asetaSijamuoto();
         return this.sananmuoto;
     }
-  
-    
+ 
     public void asetaSijamuoto() {
-        
         if (this.luku.equals(NomininLuku.YKSIKKO)) {
-            if (this.sijamuoto.equals(Sijamuoto.GENETIIVI)) {
-                super.heikkoAste();
-                this.sananmuoto += "n";
-            } else if (this.sijamuoto.equals(Sijamuoto.PARTITIIVI)) {
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "a";
-                } else {
-                    this.sananmuoto += "ä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ESSIIVI)) { 
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "na";
-                } else {
-                    this.sananmuoto += "nä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.TRANSLATIIVI)) {
-                super.heikkoAste();
-                this.sananmuoto += "ksi";
-            } else if (this.sijamuoto.equals(Sijamuoto.INESSIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "ssa";
-                } else {
-                    this.sananmuoto += "ssä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ELATIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals((Vokaali.ETU))) {
-                    this.sananmuoto += "sta";
-                } else {
-                    this.sananmuoto += "stä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ILLATIIVI)) {
-                this.sananmuoto += Character.toString(this.lekseemi.charAt(this.lekseemi.length() - 1));
-                this.sananmuoto += "n";
-            } else if (this.sijamuoto.equals(Sijamuoto.ADESSIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "lla";
-                } else {
-                    this.sananmuoto += "llä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ABLATIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "lta";
-                } else {
-                    this.sananmuoto += "llä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ALLATIIVI)) {
-                super.heikkoAste();
-                this.sananmuoto += "lle";
-            }
+            asetaYksikonSijamuoto();
         } else {
-            if (this.sijamuoto.equals(Sijamuoto.NOMINATIIVI)) {
-                super.heikkoAste();
-                this.sananmuoto += "t";
-            } else if (this.sijamuoto.equals(Sijamuoto.GENETIIVI)) {
-                this.sananmuoto += "jen";
-            } else if (this.sijamuoto.equals(Sijamuoto.PARTITIIVI)) {
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "ja";
-                } else {
-                    this.sananmuoto += "jä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ESSIIVI)) { 
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "ina";
-                } else {
-                    this.sananmuoto += "inä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.TRANSLATIIVI)) {
-                super.heikkoAste();
-                this.sananmuoto += "iksi";
-            } else if (this.sijamuoto.equals(Sijamuoto.INESSIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "issa";
-                } else {
-                    this.sananmuoto += "issä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ELATIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals((Vokaali.ETU))) {
-                    this.sananmuoto += "ista";
-                } else {
-                    this.sananmuoto += "istä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ILLATIIVI)) {
-                this.sananmuoto += "hin";
-            } else if (this.sijamuoto.equals(Sijamuoto.ADESSIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "illa";
-                } else {
-                    this.sananmuoto += "illä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ABLATIIVI)) {
-                super.heikkoAste();
-                if (this.vokaali.equals(Vokaali.ETU)) {
-                    this.sananmuoto += "ilta";
-                } else {
-                    this.sananmuoto += "illä";
-                }
-            } else if (this.sijamuoto.equals(Sijamuoto.ALLATIIVI)) {
-                super.heikkoAste();
-                this.sananmuoto += "ille";
-            }
+            asetaMonikonSijamuoto();
         }
-            
     }
     
+    
+    public void asetaYksikonSijamuoto() {
+        if (this.sijamuoto.equals(Sijamuoto.GENETIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "n";
+        } else if (this.sijamuoto.equals(Sijamuoto.PARTITIIVI)) {
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ESSIIVI)) { 
+            this.sananmuoto += "n";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.TRANSLATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ksi";
+        } else if (this.sijamuoto.equals(Sijamuoto.INESSIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ss";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ELATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "st";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ILLATIIVI)) {
+            this.sananmuoto += Character.toString(this.sananmuoto.charAt(this.lekseemi.length() - 1));
+            this.sananmuoto += "n";
+        } else if (this.sijamuoto.equals(Sijamuoto.ADESSIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ll";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ABLATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "lt";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ALLATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "lle";
+        }
+    }
+    
+    public void asetaMonikonSijamuoto() {
+        if (this.sijamuoto.equals(Sijamuoto.NOMINATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "t";
+        } else if (this.sijamuoto.equals(Sijamuoto.GENETIIVI)) {
+            this.sananmuoto += "jen";
+        } else if (this.sijamuoto.equals(Sijamuoto.PARTITIIVI)) {
+            this.sananmuoto += "j";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ESSIIVI)) { 
+            this.sananmuoto += "in";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.TRANSLATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "iksi";
+        } else if (this.sijamuoto.equals(Sijamuoto.INESSIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "iss";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ELATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ist";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ILLATIIVI)) {
+            this.sananmuoto += "hin";
+        } else if (this.sijamuoto.equals(Sijamuoto.ADESSIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ill";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ABLATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ilt";
+            this.sananmuoto += super.harmoninenA();
+        } else if (this.sijamuoto.equals(Sijamuoto.ALLATIIVI)) {
+            super.heikkoAste();
+            this.sananmuoto += "ille";
+        }
+    }
    
 }
 
--- Kaannin/src/main/java/ada/kaannin/suomi/Taipuva.java
+++ Kaannin/src/main/java/ada/kaannin/suomi/Taipuva.java
@@ -22,57 +22,62 @@ public abstract class Taipuva {
     }
     
     // voisi tehdä metodin joka päättelee onko sanassa astevaihtelu vai ei
+
     
     public String heikkoAste(){
         
-        if (!this.astevaihtelu) {
+        if (!this.astevaihtelu || this.sananmuoto.length() < 3) {
             return this.sananmuoto;
         }
         
         this.astevaihtelu = false;
-        
-        if (this.sananmuoto.length() < 3) {
-            return this.sananmuoto;
+
+        if (!onVokaali(Character.toString(this.sananmuoto.charAt(this.sananmuoto.length() - 3)))) {
+            return this.heikkoAsteKaksiKirjaintaMuuttuu();
+        } else {
+            return this.heikkoAsteYksiKirjainMuuttuu();
         }
+    }
+    
+    public String heikkoAsteYksiKirjainMuuttuu() {
+        String muokattava = this.sananmuoto.substring(this.sananmuoto.length() - 2, this.sananmuoto.length() - 1);
+        String alku = this.sananmuoto.substring(0, this.sananmuoto.length() - 2);
+        String vika = this.sananmuoto.substring(this.sananmuoto.length() - 1);
         
+
+        if (muokattava.equals("p")) {
+            muokattava = "v";
+        } else if (muokattava.equals("t")) {
+            muokattava = "d";
+        } else if (muokattava.equals("k")) {
+            muokattava = "";
+        }
+
+        this.sananmuoto = alku + muokattava + vika;
+        return this.sananmuoto;
+    }
+    
+    public String heikkoAsteKaksiKirjaintaMuuttuu() {
         String vika = this.sananmuoto.substring(this.sananmuoto.length() - 1);
         String alku = this.sananmuoto.substring(0, this.sananmuoto.length() - 3);
         String muokattava = this.sananmuoto.substring(this.sananmuoto.length() - 3, this.sananmuoto.length() - 1);
         
-       
-        
-        if (!onVokaali(Character.toString(this.sananmuoto.charAt(this.sananmuoto.length() - 3)))) {
-            if (muokattava.equals("pp") || muokattava.equals("tt") || muokattava.equals("kk")) {
-                muokattava = muokattava.substring(1);
-            } else if (muokattava.equals("mp")) {
-                muokattava = "mm";
-            } else if (muokattava.equals("nt")) {
-                muokattava = "nn";
-            } else if (muokattava.equals("nk")) {
-                muokattava = "ng";
-            } else if (muokattava.equals("lt")) {
-                muokattava = "ll";
-            } else if (muokattava.equals("rt")) {
-                muokattava = "rr";
-            }
-
-            this.sananmuoto = alku + muokattava + vika;
-            return this.sananmuoto;
-        } else {
-            muokattava = this.sananmuoto.substring(this.sananmuoto.length() - 2, this.sananmuoto.length() - 1);
-            alku = this.sananmuoto.substring(0, this.sananmuoto.length() - 2);
-
-            if (muokattava.equals("p")) {
-                muokattava = "v";
-            } else if (muokattava.equals("t")) {
-                muokattava = "d";
-            } else if (muokattava.equals("k")) {
-                muokattava = "";
-            }
-            
-            this.sananmuoto = alku + muokattava + vika;
-            return this.sananmuoto;
+        if (muokattava.equals("pp") || muokattava.equals("tt") || muokattava.equals("kk")) {
+            muokattava = muokattava.substring(1);
+        } else if (muokattava.equals("mp")) {
+            muokattava = "mm";
+        } else if (muokattava.equals("nt")) {
+            muokattava = "nn";
+        } else if (muokattava.equals("nk")) {
+            muokattava = "ng";
+        } else if (muokattava.equals("lt")) {
+            muokattava = "ll";
+        } else if (muokattava.equals("rt")) {
+            muokattava = "rr";
         }
+
+        this.sananmuoto = alku + muokattava + vika;
+        return this.sananmuoto;
     }
     
     public boolean onVokaali(String kirjain) {
@@ -82,9 +87,16 @@ public abstract class Taipuva {
         return false;
     }
     
+    public String harmoninenA() {
+        if (this.vokaali().equals(Vokaali.ETU)) {
+            return "a";
+        } else {
+            return "ä";
+        }
+    }
+    
     
      public Vokaali vokaali() {
-        
         for (int i = this.lekseemi.length() - 1; i >= 0; i++) {
             if (this.lekseemi.charAt(i) == 'y' || this.lekseemi.charAt(i) == 'ä' || this.lekseemi.charAt(i) == 'ö') {
                 return Vokaali.TAKA;
